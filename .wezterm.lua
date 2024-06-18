391deccc998ead168650352b50eba2c717991e2d
HermesSantos <hermessantos995@gmail.com>
2024-06-17 23:30:30 -0300

    adding the fullscreen on startup

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,27 +1,36 @@
 local wezterm = require 'wezterm';
+local mux = wezterm.mux
 
-return {
+local config = {
   default_prog = { '/usr/bin/zsh' },
   font = wezterm.font_with_fallback({"JetBrainsMono Nerd Font"}),
-  window_background_image = "../hrmz/Pictures/wallpapers/wallhaven-o31o2p.png",
-window_background_image_hsb = {
-      brightness = 0.005, -- Ajuste a luminosidade da imagem
+  font_size = 10,
+  window_background_image = "./Pictures/wallpapers/denys-nevozhai-D68ADLeMh5Q-unsplash.jpg",
+  window_background_image_hsb = {
+    brightness = 0.005, -- Ajuste a luminosidade da imagem
     hue = 1.0,       -- Ajuste a tonalidade da imagem
     saturation = 1.0, -- Ajuste a saturação da imagem
   },
   hide_tab_bar_if_only_one_tab = true,
-    initial_rows = 100,
-    initial_cols = 500,
-    window_decorations = "NONE",
-    window_padding = {
-      left = 0,
-      right = 0,
-      top = 0,
-      bottom = 0,
-    },
-    keys = {
-      { key = '+', mods = 'ALT|SHIFT', action = wezterm.action.SplitHorizontal },
-      { key = '_', mods = 'ALT|SHIFT', action = wezterm.action.SplitVertical },
+  initial_rows = 400,
+  initial_cols = 500,
+  window_decorations = "NONE",
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+  keys = {
+    { key = '+', mods = 'ALT|SHIFT', action = wezterm.action.SplitHorizontal },
+    { key = '_', mods = 'ALT|SHIFT', action = wezterm.action.SplitVertical },
+    { key = 'f', mods = 'ALT|SHIFT', action = wezterm.action.ToggleFullScreen },
   }
 }
 
+wezterm.on("gui-startup", function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():perform_action(wezterm.action.ToggleFullScreen, pane)
+end)
+
+return config
